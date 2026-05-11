b6181f261af9b7a24641240857d0d3998c4a6ab7
Niken Lismiati <nikenlismiati04@gmail.com>
2026-05-11 19:44:54 +0700

    fix(auth): set initial state for auth controller

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,32 +1,50 @@
 import 'package:getx_countries_app/services/storage_service.dart';
 import 'package:get/get.dart';
 import '../services/auth_service.dart';
+class AuthController extends GetxController
+    with StateMixin<void> {
 
-class AuthController extends GetxController with StateMixin<void> {
   final AuthService _service = AuthService();
 
+  @override
+  void onInit() {
+    change(null, status: RxStatus.success());
+    super.onInit();
+  }
+
   Future<void> login(String user, String pass) async {
+
     change(null, status: RxStatus.loading());
 
     try {
+
       final result = await _service.login(user, pass);
 
       if (result) {
+
         await StorageService.saveLogin(user);
 
         change(null, status: RxStatus.success());
 
         Get.offAllNamed('/home');
+
       } else {
+
         change(
           null,
-          status: RxStatus.error("Invalid credentials"),
+          status: RxStatus.error(
+            "Invalid credentials",
+          ),
         );
       }
+
     } catch (e) {
+
       change(
         null,
-        status: RxStatus.error("Something went wrong"),
+        status: RxStatus.error(
+          "Something went wrong",
+        ),
       );
     }
   }
