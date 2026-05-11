246f2ed651ededaa56ace4c2efbeec3445884746
Niken Lismiati <nikenlismiati04@gmail.com>
2026-05-11 19:29:33 +0700

    refactor(auth) : update authentication flow using getx statemixin

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -2,25 +2,32 @@ import 'package:getx_countries_app/services/storage_service.dart';
 import 'package:get/get.dart';
 import '../services/auth_service.dart';
 
-
-class AuthController extends GetxController {
+class AuthController extends GetxController with StateMixin<void> {
   final AuthService _service = AuthService();
 
-  var isLoading = false.obs;
-
   Future<void> login(String user, String pass) async {
-    isLoading.value = true;
+    change(null, status: RxStatus.loading());
 
-    final result = await _service.login(user, pass);
+    try {
+      final result = await _service.login(user, pass);
 
-    isLoading.value = false;
+      if (result) {
+        await StorageService.saveLogin(user);
 
-    if (result) {
-      await StorageService.saveLogin(user);
+        change(null, status: RxStatus.success());
 
-      Get.offAllNamed('/home');
-    } else {
-      Get.snackbar("Login Failed", "Invalid credentials");
+        Get.offAllNamed('/home');
+      } else {
+        change(
+          null,
+          status: RxStatus.error("Invalid credentials"),
+        );
+      }
+    } catch (e) {
+      change(
+        null,
+        status: RxStatus.error("Something went wrong"),
+      );
     }
   }
 }
