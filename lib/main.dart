71d5ee468a6959ebca572188a96d1cff6c5b1861
Niken Lismiati <nikenlismiati04@gmail.com>
2026-05-11 22:15:58 +0700

    feat: implement auto login redirect

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,20 @@ import 'package:getx_countries_app/services/storage_service.dart';
 import 'package:toastification/toastification.dart';
 
 import 'app/routes/app_pages.dart';
+import 'app/routes/app_routes.dart';
 import 'app/theme/theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await StorageService.init();
-  runApp(const App());
+  final isLoggedIn = StorageService.isLoggedIn();
+
+  runApp(App(isLoggedIn: isLoggedIn));
 }
 
 class App extends StatelessWidget {
-  const App({super.key});
+  final bool isLoggedIn;
+  const App({super.key, required this.isLoggedIn});
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +26,7 @@ class App extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         themeMode: ThemeMode.dark,
         darkTheme: AppTheme.darkTheme,
-        initialRoute: AppPages.initial,
+        initialRoute: isLoggedIn ? Routes.home : Routes.login,
         getPages: AppPages.pages,
       ),
     );
