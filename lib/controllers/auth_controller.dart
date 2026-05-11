c319120902e3c8c86347bd4eb87c8ac0eca758bb
Niken Lismiati <nikenlismiati04@gmail.com>
2026-05-11 21:32:36 +0700

    chore: remove unused comments from auth controller

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,55 +1,7 @@
 import 'package:getx_countries_app/services/storage_service.dart';
 import 'package:get/get.dart';
+import '../app/routes/app_routes.dart';
 import '../services/auth_service.dart';
-// class AuthController extends GetxController
-//     with StateMixin<void> {
-
-//   final AuthService _service = AuthService();
-
-//   @override
-//   void onInit() {
-//     change(null, status: RxStatus.success());
-//     super.onInit();
-//   }
-
-//   Future<void> login(String user, String pass) async {
-
-//     change(null, status: RxStatus.loading());
-
-//     try {
-
-//       final result = await _service.login(user, pass);
-
-//       if (result) {
-
-//         await StorageService.saveLogin(user);
-
-//         change(null, status: RxStatus.success());
-
-//         Get.offAllNamed('/home');
-
-//       } else {
-
-//         change(
-//           null,
-//           status: RxStatus.error(
-//             "Invalid credentials",
-//           ),
-//         );
-//       }
-
-//     } catch (e) {
-
-//       change(
-//         null,
-//         status: RxStatus.error(
-//           "Something went wrong",
-//         ),
-//       );
-//     }
-//   }
-// }
-
 
 
 class AuthController extends GetxController
@@ -85,7 +37,7 @@ class AuthController extends GetxController
 
         change(null, status: RxStatus.success());
 
-        Get.offAllNamed('/home');
+        Get.offAllNamed(Routes.home);
 
       } else {
 
