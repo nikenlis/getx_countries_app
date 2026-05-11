bc7e09cbdd69fc6406a2c9bad0ada0b834b2e216
Niken Lismiati <nikenlismiati04@gmail.com>
2026-05-11 21:33:30 +0700

    feat: implement country controller with getx state handling

---
+++ lib/controllers/country_controller.dart
@@ -0,0 +1,46 @@
+import 'package:get/get.dart';
+
+import '../models/country_model.dart';
+import '../services/country_service.dart';
+
+class CountryController extends GetxController
+    with StateMixin<List<CountryModel>> {
+
+  final CountryService _service =
+      CountryService();
+
+  @override
+  void onInit() {
+    super.onInit();
+
+    fetchCountries();
+  }
+
+  Future<void> fetchCountries() async {
+
+    change(
+      [],
+      status: RxStatus.loading(),
+    );
+
+    try {
+
+      final countries =
+          await _service.fetchCountries();
+
+      change(
+        countries,
+        status: RxStatus.success(),
+      );
+
+    } catch (e) {
+
+      change(
+        [],
+        status: RxStatus.error(
+          e.toString(),
+        ),
+      );
+    }
+  }
+}
