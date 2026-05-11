4d5fe61b0c2577545aaed86c4fa0313d86bf61af
Niken Lismiati <nikenlismiati04@gmail.com>
2026-05-11 21:31:03 +0700

    feat(home) : add named routes and country bindings

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -4,6 +4,7 @@ import 'package:getx_countries_app/app/routes/app_routes.dart';
 import '../../views/home_page.dart';
 import '../../views/login_page.dart';
 import '../binding/auth_binding.dart';
+import '../binding/country_binding.dart';
 
 class AppPages {
   static const initial = Routes.login;
@@ -15,6 +16,6 @@ class AppPages {
       binding: AuthBinding(),
     ),
 
-    GetPage(name: '/home', page: () => HomePage()),
+    GetPage(name: '/home', page: () => HomePage(), binding: CountryBinding()),
   ];
 }
